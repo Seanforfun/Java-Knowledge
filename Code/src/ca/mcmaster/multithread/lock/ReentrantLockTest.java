7acd66152a6aa7e0fb6db364b5b5be5664b27547
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-04-08 18:15:41 -0400

    [Conclusion][Function add]
    1.Add conclusion about CAS and ReentrantLock.
    2. Add test to ReentrantLock.

--- Code/src/ca/mcmaster/multithread/lock/ReentrantLockTest.java
+++ Code/src/ca/mcmaster/multithread/lock/ReentrantLockTest.java
@@ -1,23 +1,35 @@
 package ca.mcmaster.multithread.lock;
 
-public class ReentrantLockTest extends Thread {
-	private synchronized void get(){
-		System.out.println(Thread.currentThread() + " :get");
-		set();
-	}
-	private void set() {
-		for(int i = 0; i < 5; i++)
-			System.out.println(Thread.currentThread() + " :set");
-	}
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest implements Runnable {
+	private ReentrantLock reentrantLock = new ReentrantLock(false);
+	private int value = 0;
 	@Override
 	public void run() {
-		for(int i = 0; i < 5; i++)
-			get();
+		reentrantLock.lock();
+		for(int i = 0; i < 3; i++){
+			updateValue();
+		}
+		reentrantLock.unlock();
 	}
 	
+	private void updateValue() {
+		reentrantLock.lock();
+		System.out.println(Thread.currentThread() + " " + value++);
+		reentrantLock.unlock();
+	}
+
 	public static void main(String[] args) {
-		for(int i = 0; i < 2; i++){
-			new ReentrantLockTest().start();
+		ReentrantLockTest rtLock = new ReentrantLockTest();
+		for(int i = 0; i < 10; i ++){
+			Thread thread = new Thread(rtLock);
+			if(i == 3){
+				thread.setPriority(Thread.MAX_PRIORITY);
+			}else if (i == 0) {
+				thread.setPriority(Thread.MIN_PRIORITY);
+			}
+			thread.start();
 		}
 	}
 }
